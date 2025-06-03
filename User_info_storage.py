20cd500ffa7ec9b8c0fa8aef007c5ea9cfe0cb40
vishnu-arU <vishnuarun25@gmail.com>
2025-06-03 11:41:43 +0530

    firstcommit

---
+++ User_info_storage.py
@@ -0,0 +1,18 @@
+# creating the user name and details &storing in the variable
+
+user_name= "Vishnulal"
+user_id = "001"
+user_age = 25
+user_height ="179" +" "+"cm"
+
+# printing username
+print(f"the username is {user_name}")
+
+# printing the user_id
+print(f"the user_id is {user_id}")
+
+#printing the user age
+print(f"the userage is {user_age}")
+
+#printing the user_height
+print(f"the userheight is {user_height}")
