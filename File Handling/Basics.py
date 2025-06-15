dcb2c173e3a36f4721e6a0133265314b1f3176ab
vishnu-arU <vishnuarun25@gmail.com>
2025-06-15 22:44:46 +0530

    lastcommited

---
+++ File Handling/Basics.py
@@ -0,0 +1,16 @@
+# syntax
+
+file =open("filename.txt","modes")  # this is the syntax
+
+
+Modes
+
+# "r"= read only( file must exist)
+# "w"= write only( overwrites or creates)
+# "a"= append only( adds to the end of file)
+# "r+"= read & write( file must exist)
+# "w+"= write & read(overwrites or cretes)
+# "a+"= append &read(creates if doesnot exists)
+# "rb"=read binary
+# "wb"=write binary
+# "ab"=append binary
